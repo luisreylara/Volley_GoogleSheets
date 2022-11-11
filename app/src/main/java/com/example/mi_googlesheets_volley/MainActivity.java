a035e994d893cebc41cc401d1892fc7cf14b024b
Luis Rey Lara <luisreylara@hotmail.com>
2022-11-11 09:38:17 -0600

    POST completo

--- app/src/main/java/com/example/mi_googlesheets_volley/MainActivity.java
+++ app/src/main/java/com/example/mi_googlesheets_volley/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.mi_googlesheets_volley;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,23 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://projects.vishnusivadas.com/testing/read.php";
+        //String url ="https://projects.vishnusivadas.com/testing/read.php";
+        String url ="https://datamoviles.tk/volley/write.php";
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         // textView.setText("Response is: "+ response.substring(0,500));
-                        textView.setText("Response is: "+ response );
+                        textView.setText("Response is: "+ response+" :\n "+url );
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 textView.setText("That didn't work!");
             }
-        });
+        }){
+            protected Map<String, String> getParams(){
+                Map<String, String> paramV = new HashMap<>();
+                paramV.put("param","abc");
+                return paramV;
+            }
+        };
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
